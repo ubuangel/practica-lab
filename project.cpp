73ca4028dfcec6cacea92716d5e444c11eded43b
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-17 12:16:20 -0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,5 +1,7 @@
 //project euler problem 3
 //https://projecteuler.net/archives
+#include<iostream>
+using namespace std;
 int main(){
 n = 600851475143;		
 i = 2;
@@ -42,4 +44,27 @@ int main(){
 		}
 }
 	cout<<sum;}
+
+//primos
+int main()
+{
+//for (int j=2;j<=100;j++){//por definicion el 1 no es primo, se empieza en 2
+ //int a=0;
+ int n;
+ cout<<"numero";cin>>n;
+ for( int j = 2; j <=n; j++)
+ {
+     
+ int a=0;
+ for(int i=1;i<=n;i++)//divide a j entre los números del 1 al 100
+ 
+ {
+    if(j%i==0) // si num1 módulo de i es 0, incrementamos a en 1.
+    a++;
+ }
+ if (a==2){ //si solo tiene dos números divisores entonces es primo y se imprime
+ cout<<" "<<j;
+        }
+    }
+}
 
