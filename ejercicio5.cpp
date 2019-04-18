dc3897e5359aa5981d3c38da9f7e9d8f511b23b4
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-18 10:45:41 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,1 +1,32 @@
 double x = rand () / ( double ) RAND_MAX , y = rand () / ( double ) RAND_MAX ;
+//ejercicio 5.2
+int dartsInCircle = 0;
+	 for ( int i = 0; i < n ; ++ i ) {
+
+	double x = rand () / ( double ) RAND_MAX , y = rand () / ( double )
+	RAND_MAX ;
+
+		if ( sqrt ( x * x + y * y ) < 1 ) {
+
+		++ dartsInCircle ;
+
+	}
+}
+//ejercicio 5.3
+ 
+double computePi ( const int n ) {
+
+	srand ( time (0) ) ;
+
+
+int dartsInCircle = 0;
+	for ( int i = 0; i < n ; ++ i ) {
+		double x = rand () / ( double ) RAND_MAX , y = rand () / ( double )
+		RAND_MAX ;
+	if ( sqrt ( x * x + y * y ) < 1 ) {
+		++ dartsInCircle ;
+		}
+	}
+// r ^2 is 1 , and a /4 = dartsInCircle /n , yielding this for pi :
+return dartsInCircle / static_cast < double >( n ) * 4;
+}
