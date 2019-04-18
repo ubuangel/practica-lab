0b27086e70494601a1ee83fd234aeb61d5bfc101
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-18 10:37:51 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -13,3 +13,11 @@ int main ()
 	f(a);
 	f () ;
 }
+//ejercicio 3
+
+int sum ( const int x , const int y , const int z ) {
+return x + y + z ;
+}
+int sum ( const int a , const int b , const int c , const int d ) {
+return a + b + c + d ;
+}
