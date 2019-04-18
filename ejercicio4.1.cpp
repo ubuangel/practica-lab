beae538ceef2a18661d16b8570ea7bb772975f90
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-18 10:41:49 -0500

    Update ejercicio4.1.cpp

--- ejercicio4.1.cpp
+++ ejercicio4.1.cpp
@@ -4,3 +4,9 @@ return x + y ;
 double sum ( const double x , const double y ) {
 return x + y ;
 }
+//ejericio 4.4
+int sum ( const int a , const int b , const int c = 0 , const int d = 0)
+{
+
+return a + b + c + d ;
+}
