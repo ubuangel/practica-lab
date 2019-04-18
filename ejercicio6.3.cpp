c775653d1d7980183864a4798784fda41044d474
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-18 10:54:29 -0500

    Delete ejercicio6.3.cpp

--- ejercicio6.3.cpp
+++
@@ -1,9 +0,0 @@
-void transpose ( const int input [][ LENGTH ] , int output [][ WIDTH ]) {
-
-	for ( int i = 0; i < WIDTH ; ++ i ) {
-
-		for ( int j = 0; j < LENGTH ; ++ j ) {
-		output [ j ][ i ] = input [ i ][ j ];
-			}
-	}
-}
