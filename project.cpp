c252c5181d2e7696d49918b753bd48a36e5cfb38
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-22 09:53:11 -0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -67,11 +67,8 @@ int main()
       cout << next <<" \n";
       cout <<"\n" <<sum<<" \n";
    }
-
    return 0;
 }
-
-
 //project 4
 
 int palindromo3(int n)
@@ -111,37 +108,30 @@ int main()
     }
 }
 //eejrcicio 5
-void dividir(double n, int k)
+void dividir(long n, int k)
 {
-    for(int i=100;i<n;i++)
+    for(int i=2520;i<n;i++)
     {
         int c=0;
         for(int j=1;j<=k;j++)
         {
-            if(i%j==0)
-            {
-                c++;
-            }
-        }
-
-        if(c==k)
-        {
-            cout<<i<<",";
+            if(i%j==0){
+                c++;}
+                	if(c==k){
+                		cout<<i<<",";}
         }
     }
 }
 
-
 int main()
 {
-    double n;
+    long n;
     int k;
     cout<<"numero: ";
     cin>>n;
     cout<<"divisores: ";
     cin>>k;
     dividir(n,k);
-}
 //ejercicio 6
 #include<math.h>
 int main(){
