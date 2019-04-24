1c25ae52d89e2a603c982994f57a22d4e7021b4c
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-24 08:22:17 -0500

    Add files via upload

---
+++ mit3.cpp
@@ -0,0 +1,56 @@
+//2.1
+class Point
+{
+	private:
+	int x,y;
+
+public:
+	Point(int u,int v):x(u),y(v){}
+	int getX(){return x;}
+	int getY(){return y;}
+
+	void doubleVal(){
+	x*=2; y*=2;
+	}
+};
+	int main()
+	{
+	 	Point myPoint(5,3);
+	myPoint.doubleVal();
+	cout<<myPoint.getX()<<" ,"<<myPoint.getY()<<"\n";
+	return 0;
+	}
+//2.3
+ class Point{
+        
+    public:
+        int x,y;
+    Point(int u,int v):x(u),y(v){}
+    int getX(){return x;}
+    int getY(){return y;}
+    };
+int main()
+{
+    Point p(5,3);
+    cout<<p.x<<" "<<p.y<<"\n";
+return 0;
+    
+}
+//2.6
+class Point{
+    private: int x,y;
+    public:
+    Point(int u,int v):x(u),y(v){}
+    int getX(){return x;}
+    
+    int getY(){return y;}
+    
+};
+int main()
+{
+    Point *p=new Point(5,3);
+
+
+cout<<p->getX()<<" "<<p->getY();
+delete p;
+return 0;}
