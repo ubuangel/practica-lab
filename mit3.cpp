c9b3455c98b5c2edd651f1da0fe5251001b995e1
ubuangel <49200646+ubuangel@users.noreply.github.com>
2019-04-24 10:58:09 -0500

    Update mit3.cpp

--- mit3.cpp
+++ mit3.cpp
@@ -20,6 +20,30 @@ public:
 	cout<<myPoint.getX()<<" ,"<<myPoint.getY()<<"\n";
 	return 0;
 	}
+//2.2
+#include<iostream>
+using namespace std;
+
+
+class Point{
+private:
+	int x,y;
+public:
+	Point(int u, int v):x(u),y(v){}
+	int getX(){return x;}
+	int getY(){return y;}
+	void setX(int newX){x=newX;}
+
+
+};
+
+int main()
+{
+	Point p(5,3);
+	p.setX(9001);
+	cout<<p.getX()<<" "<<p.getY();
+	return 0;
+}
 //2.3
  class Point{
         
